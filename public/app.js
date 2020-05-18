827a3cc75ad7cdac07423e262e311954d4bf7d52
Shaoyong Feng <feng.sha@husky.neu.edu>
2020-05-17 22:47:52 -0400

    added babel-react

---
+++ public/app.js
@@ -0,0 +1,6 @@
+const element = /*#__PURE__*/React.createElement("div", {
+  title: "Outer div"
+}, /*#__PURE__*/React.createElement("h1", {
+  className: "header"
+}, " Chapter 2: Hello MERN "));
+ReactDOM.render(element, document.getElementById('contents'));
---
+++ src/app.jsx
@@ -0,0 +1,6 @@
+const element = (
+    <div title = "Outer div">
+        <h1 className = 'header'> Chapter 2: Hello MERN </h1>  
+    </div>
+);
+ReactDOM.render(element, document.getElementById('contents'));
